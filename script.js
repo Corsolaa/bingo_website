55b88e653dfef634f064eb79915d88ed98dba896
John <bruno.bouwman4@gmail.com>
2023-01-21 15:17:04 +0100

    Update script.js
    removed some errors and added a chance to get the sound file

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function createBall() {
 
 function changeBall() {
     const ball_array = balls.querySelectorAll(".ball");
-    let index = Math.floor(Math.random()*ball_colors.length);
+    let index = Math.floor(Math.random() * ball_colors.length);
     let ball_number = getBallNumber();
     let ball = ball_array[ball_number - 1];
     ball.innerText = ball_number;
@@ -48,20 +48,16 @@ function changeBall() {
     if (ball_number < 10) {
         ball_text = "&nbsp;" + ball_number
     }
-<<<<<<< HEAD
     if (ball_number === 69) {
-        let audio = new Audio("69-mcdonalds.mp3");
-        audio.volume = 0.08;
-        audio.play();
-
+        if (Math.floor(Math.random() * (21 - 1) + 1) === 20) {
+            let audio = new Audio("69-mcdonalds.mp3");
+            audio.volume = 0.08;
+            audio.play();
+        }
         grab_ball.innerHTML = "Funny number - " + ball_text;
-    }
-    else {
+    } else {
         grab_ball.innerHTML = "Pick ball - " + ball_text;
     }
-=======
-    grab_ball.innerHTML = "Pick ball - " + ball_text;
->>>>>>> 7322714d0c2810442b9ce4be32d3ade2ce8965e0
 }
 
 function getBallNumber() {
@@ -80,7 +76,7 @@ function getBallNumber() {
 }
 
 function loadBalls() {
-    for (let i = 0; i<ball_numbers[1]; i++) {
+    for (let i = 0; i < ball_numbers[1]; i++) {
         createBall();
     }
 }
